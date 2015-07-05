2f957f3d177f7d54fb3df773ba489e050bdf49d4
Dominic Moran <dominic.moran@webinnovate.ie>
2015-07-05 20:45:33 +0100

    register

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -37,69 +37,54 @@
 angular.module('application').controller('LoginController', LoginController);
 LoginController.$inject = ['$scope', '$stateParams', '$state', '$controller'];
 function LoginController($scope, $stateParams, $state, $controller) {
+  angular.extend(this, $controller('DefaultController', {
+      $scope: $scope, $stateParams: $stateParams, $state: $state
+      })
+  );
+  $scope.Login = function(username, password, code){
+    var response = {};
+    console.log('...in Login...', response);
+    $scope.showstatus = '';
+    console.log('u:',username, 'p:',password, 's.u',$scope.username);
+    if ($scope.username !== 'aaa') {
+      $scope.showstatus = 'Invalid Username ';
+    }
+    if ($scope.password !== 'bbb') {
+      $scope.showstatus += ' Invalid Password';
+    }
 
-    angular.extend(this, $controller('DefaultController', {$scope: $scope, $stateParams: $stateParams, $state: $state}));
+  };
+}
 
-    // localStorage.removeItem("saved_tasks");
-    // localStorage.removeItem("all_tags");
+angular.module('application').controller('RegisterController', RegisterController);
+RegisterController.$inject = ['$scope', '$stateParams', '$state', '$controller'];
+function RegisterController($scope, $stateParams, $state, $controller) {
+  angular.extend(this, $controller('DefaultController', {
+      $scope: $scope, $stateParams: $stateParams, $state: $state
+      })
+  );
+  $scope.Register = function(username, password, code){
+    var response = {};
+    console.log('...in Register...', response);
+    $scope.showstatus = '';
+    console.log('u:',username, 'p:',password, 's.u',$scope.username);
+    if ($scope.username !== 'aaa') {
+      $scope.showstatus = 'Invalid Username ';
+    }
+    if ($scope.password !== 'bbb') {
+      $scope.showstatus += ' Invalid Password';
+    }
 
-    // $scope.editMode = false;
-
-    // $scope.showstatus = 'incomplete';
-
-    // $scope.showtag = {};
-
-    // $scope.tasks = JSON.parse(localStorage.getItem("saved_tasks"));
-    // if (typeof $scope.tasks === 'undefined' || $scope.tasks === null){
-    //   $scope.tasks = [];
-    // }
-
-    // $scope.allTags = JSON.parse(localStorage.getItem("all_tags"));
-    // if (typeof $scope.allTags === 'undefined' || $scope.allTags === null){
-    //   $scope.allTags = [];
-    // }
-
-    $scope.Login = function(username, password, code){
-      var response = {};
-      console.log('...in Login...', response);
-      $scope.showstatus = '';
-      console.log('u:',username, 'p:',password, 's.u',$scope.username);
-      if ($scope.username !== 'aaa') {
-        $scope.showstatus = 'Invalid Username ';
-      }
-      if ($scope.password !== 'bbb') {
-        $scope.showstatus += ' Invalid Password';
-      }
-      // response.title = title;
-      // response.body = body;
-      // response.status = '';
-      // response.color = '';
-      // // Combine all task tag arrays into allTags array
-      // $scope.allTags = $scope.allTags.concat(task.tags);
-
-      // // Strip duplicate tags
-      // $scope.allTags = $scope.allTags.filter(function(element, index) {
-      //   return $scope.allTags.indexOf(element) == index;
-      // }); 
-
-      // // Set unique ID from current timestamp
-      // task.id = Date.now();
-
-      // $scope.tasks.push(task);
-
-      // localStorage.setItem("all_tags", JSON.stringify($scope.allTags));
-      // localStorage.setItem("saved_tasks", JSON.stringify($scope.tasks));
-    };
- }
+  };
+}
 
 angular.module('application').controller('TestController', TestController);
 TestController.$inject = ['$scope', '$stateParams', '$state', '$controller'];
 function TestController($scope, $stateParams, $state, $controller) {
-
-    angular.extend(this, $controller('DefaultController', {$scope: $scope, $stateParams: $stateParams, $state: $state}));
-
-    // localStorage.removeItem("saved_tasks");
-    // localStorage.removeItem("all_tags");
+  angular.extend(this, $controller('DefaultController', {
+    $scope: $scope, $stateParams: $stateParams, $state: $state
+    })
+  );
 
     $scope.editMode = false;
 
